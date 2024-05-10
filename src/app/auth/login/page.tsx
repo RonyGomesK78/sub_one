8297ac3fb339e42bb69da644905659799f0637d2
Romilton Gomes <romilton.gomes@chuva.io>
2024-05-10 22:23:34 -0100

    put the bg image on the Image next component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import { useEffect, useContext, useState } from 'react';
-
 import Image from 'next/image';
-
 import { useAppSelector } from "@/lib/redux/hooks";
 import { AuthContext } from '@/contexts/AuthContext';
-
 import { users } from '@/lib/redux/features/auth/usersSlice';
 import LoginForm from '@/components/LoginForm';
-
 import { LoginUser } from '@/interfaces/User';
-
 import bg from '../../../../public/bgImage.jpg';
 import logo from '../../../assets/nuno rocha.png';
 import { RootState } from '@/lib/redux/store';
@@ -19,11 +14,9 @@ import Toast from '@/components/Toast';
 
 export default function Login() {
   const { signIn } = useContext(AuthContext);
-
   const user = useAppSelector((state: RootState) => users(state));
-
   const [showToast, setShowToast] = useState(false);
-  
+
   const handleLogin = (data: LoginUser) => {
     signIn({
       email: data.email,
@@ -35,7 +28,7 @@ export default function Login() {
     if (user.createdStatus === 'failed') {
       setShowToast(true);
     }
-  }, [user.createdStatus])
+  }, [user.createdStatus]);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -47,8 +40,14 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex justify-center bg-cover bg-center h-screen" style={{ backgroundImage: `url(${bg.src})` }}>
-        <div className="flex justify-center items-center h-full w-full">
+      <div className="flex justify-center h-screen relative">
+        <Image
+          src={bg}
+          alt='Background Image'
+          priority={true}
+          fill={true}
+        />
+        <div className="absolute inset-0 flex justify-center items-center">
           <div className="flex flex-col justify-center items-center bg-white py-10 rounded shadow-lg md:w-3/4 lg:w-3/5 xl:w-2/5 2xl:w-1/4 w-full">
             <Image
               src={logo}
@@ -60,12 +59,12 @@ export default function Login() {
             <LoginForm handleLogin={handleLogin} />
           </div>
         </div>
-        <Toast 
-          message={"Credências inválidas"}
-          showToast={showToast}
-          color="bg-red-500"
-      />
       </div>
+      <Toast 
+        message={"Credências inválidas"}
+        showToast={showToast}
+        color="bg-red-500"
+      />
     </>
   );
 }
